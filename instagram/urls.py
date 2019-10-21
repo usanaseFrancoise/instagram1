c01dd5e2a2b53a0dbd9fb918870903261c11de32
usanase Francoise <fusanasencoise@gmail.com>
2019-10-21 16:47:26 +0200

    first commit

--- instagram/urls.py
+++ instagram/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('account.urls')),
+    url(r'^', include('account.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout,{"next_page":'/'}),
     url(r'^tinymce/', include('tinymce.urls'))
