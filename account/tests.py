cb9b9bb725e6f5ea493b90b354934db8467b466a
usanase Francoise <fusanasencoise@gmail.com>
2019-10-22 12:50:47 +0200

    save test

--- account/tests.py
+++ account/tests.py
@@ -7,21 +7,26 @@ from .models import Profile,Image,Comment
 # Create your tests here.
 
 class ImageTestClass(TestCase):
+
     '''
     set up method
     '''
-def setUp(self):
-   self.user = User.objects.create_user('fanny')
-   self.user.save()
+    def setUp(self):
+        self.user = User.objects.create_user('fanny')
+        self.user.save()
 
-def test_instance(Self):
-    self.image.save()
-    self.assertTrue(isinstance(self.Image.Image))
+    def test_instance(Self):
 
-def test_save_method(self):
-    self.image.save_image()
-    images=Image.objects.all()
-    self.assertTrue(len(images)>0)
+        self.image.save()
+        self.assertTrue(isinstance(self.Image.Image))
+
+    def test_save_method(self):
+
+        self.image.save_image()
+        images=Image.objects.all()
+        self.assertTrue(len(images)>0)
+
+    
 
 
 
